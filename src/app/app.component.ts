4595861d0c50c79f70baf52d60f6d00fca6e460d
Serhat Turkman <SERHAT.TURKMAN@tarnet.com.tr>
2019-01-21 15:30:54 +0300

    asd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'AngularTutorial3';
+  courses = [];
+  viewMode = 'map';
+  task = {
+    title: 'task1',
+    asignee: {
+      name: 'John'
+    }
+  };
+  onAdd() {
+
+    this.courses.push({ id: (this.courses.length + 1), name: 'course ' + (this.courses.length + 1)});
+  }
+
+  onRemove(course) {
+    const index = this.courses.indexOf(course)
+    this.courses.splice(index, 1);
+  }
+
+  loadCourses() {
+    this.courses = [
+      { id: 1, name: 'course 1'},
+      { id: 1, name: 'course 2'},
+      { id: 1, name: 'course 3'}
+    ];
+  }
+
+  trackCourse(index, course) {
+    return course ? course.id : undefined;
+  }
 }
